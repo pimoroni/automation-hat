a320473b0327f224be1f614fcdf5509521a8c64c
Hel Gibbons <hel@pimoroni.com>
2024-04-26 12:34:22 +0100

    Update Automation HAT Mini examples

--- examples/hat-mini/output.py
+++ examples/hat-mini/output.py
@@ -14,7 +14,7 @@ Install with: sudo apt install python{v}-pil
 """.format(v="" if sys.version_info.major == 2 else sys.version_info.major))
     sys.exit(1)
 
-import ST7735 as ST7735
+import st7735
 
 IMAGE_PATH = os.path.join(os.path.dirname(__file__), "images")
 
@@ -45,11 +45,11 @@ def draw_states(channels):
 
 
 # Create ST7735 LCD display class.
-disp = ST7735.ST7735(
+disp = st7735.ST7735(
     port=0,
-    cs=ST7735.BG_SPI_CS_FRONT,
-    dc=9,
-    backlight=25,
+    cs=st7735.BG_SPI_CS_FRONT,
+    dc="GPIO9",
+    backlight="GPIO25",
     rotation=270,
     spi_speed_hz=4000000
 )
